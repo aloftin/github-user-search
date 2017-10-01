b7a44327d00cc047491c25bf6d3275e1a7fb84d1
Adam Loftin <loftin.adam@gmail.com>
2017-10-01 16:09:15 -0500

    Enzyme configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./components/App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
