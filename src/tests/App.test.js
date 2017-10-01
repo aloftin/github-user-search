fe9566159a111dab83a1d70fe4539424cbd713dd
Adam Loftin <loftin.adam@gmail.com>
2017-10-01 17:23:07 -0500

    More project directory clean up

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "./components/App";
+import App from "../components/App";
 
 it("renders without crashing", () => {
   const component = shallow(<App />);
   expect(component.exists()).toEqual(true);
 });
+
+// it changes userfound state to false if bad username is given
+// - mock API call
