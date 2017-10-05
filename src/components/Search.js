021e9a73ac050b91c3018b7764cdd2ac2c0ffb46
Adam Loftin <loftin.adam@gmail.com>
2017-10-05 09:37:30 -0500

    Extracted a SearchInput component from Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import searchIcon from '../images/search.svg';
-
-const Input = styled.input`
-  height: 15px;
-  width: 90%;
-  border: solid 1px #d8e3ea;
-  color: #7a7a86;
-  font-size: 16px;
-  padding: 10px;
-  border-radius: 25px;
-  margin-bottom: 20px;
-  padding-left: 35px;
-  background: url(${searchIcon}) no-repeat scroll 7px 7px;
-  background-size: 22px;
-
-  ::placeholder {
-    color: #d8e3ea;
-  }
-`;
+import SearchInput from './SearchInput';
+import Button from './Button';
 
 class Search extends React.Component {
   constructor(props) {
@@ -30,6 +12,7 @@ class Search extends React.Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
   handleInputChange = e => {
@@ -50,17 +33,22 @@ class Search extends React.Component {
     }
   };
 
+  handleButtonClick = e => {
+    e.preventDefault();
+
+    this.props.searchForUser(this.state.searchText);
+  };
+
   render() {
     return (
-      <Input
-        autoFocus
-        name="searchText"
-        type="text"
-        placeholder="Search for users..."
-        value={this.state.searchText}
-        onChange={this.handleInputChange}
-        onKeyPress={this.handleKeyPress}
-      />
+      <div>
+        <SearchInput
+          value={this.state.searchText}
+          onChange={this.handleInputChange}
+          onKeyPress={this.handleKeyPress}
+        />
+        <Button round content="Go!" onClick={this.handleButtonClick} />
+      </div>
     );
   }
 }
---
+++ src/components/SearchInput.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import searchIcon from '../images/search.svg';
+
+const Input = styled.input`
+  height: 15px;
+  width: 70%;
+  border: solid 1px #d8e3ea;
+  color: #7a7a86;
+  font-size: 16px;
+  padding: 10px;
+  border-radius: 25px;
+  margin: 0px 5px 20px 0px;
+  padding-left: 35px;
+  background: url(${searchIcon}) no-repeat scroll 7px 7px;
+  background-size: 22px;
+  display: inline-block;
+
+  ::placeholder {
+    color: #d8e3ea;
+  }
+`;
+
+const SearchInput = ({ value, onChange, onKeyPress }) => {
+  return (
+    <Input
+      autoFocus
+      name="searchText"
+      type="text"
+      placeholder="Search for users..."
+      value={value}
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+    />
+  );
+};
+
+export default SearchInput;
