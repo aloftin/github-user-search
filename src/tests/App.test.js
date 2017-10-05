e92616a6f5d167eb8bfe4fe5cb5141b63be3b24d
Adam Loftin <loftin.adam@gmail.com>
2017-10-04 23:00:02 -0500

    Tests for searching for a user

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
+import nock from 'nock';
 import App from '../components/App';
 import Search from '../components/Search';
 import Followers from '../components/Followers';
+
+// make axios play nice with nock :)
+axios.defaults.adapter = httpAdapter;
 
 it('renders without crashing', () => {
   mount(<App />);
@@ -12,13 +18,6 @@ describe('App', () => {
   it('should render Search and Followers', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.containsAllMatchingElements([<Search />, <Followers />])).toEqual(true);
-  });
-
-  it('should render a load more button if there are additional pages of followers', () => {
-    const app = shallow(<App />);
-    app.setState({ hasMoreFollowers: true });
-
-    expect(app.find('button')).toHaveLength(1);
   });
 
   it('passes resetFollowers to Search', () => {
@@ -39,5 +38,63 @@ describe('App', () => {
 
     wrapper.find(Search).simulate('change');
     expect(wrapper.state('followers')).toEqual([]);
+  });
+
+  it('should render a load more button if there are additional pages of followers', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ hasMoreFollowers: true });
+
+    expect(wrapper.find('button')).toHaveLength(1);
+  });
+
+  describe('searchForUser', () => {
+    describe('when username is invalid', () => {
+      it('should set userFound state to false', () => {
+        const badUsername = 'badusername';
+
+        nock('https://api.github.com')
+          .get(`/users/${badUsername}`)
+          .reply(404, { message: 'Not Found' });
+
+        const wrapper = shallow(<App />);
+        wrapper.instance().searchForUser(badUsername);
+
+        // need setTimeout to process next tick
+        setTimeout(() => {
+          expect(wrapper.state('userFound')).toEqual(false);
+        }, 0);
+      });
+    });
+
+    describe('when username is valid', () => {
+      const goodUsername = 'goodusername';
+
+      beforeEach(() => {
+        nock('https://api.github.com')
+          .get(`/users/${goodUsername}`)
+          .reply(200, { id: 123 });
+
+        nock('https://api.github.com')
+          .get(`/user/123/followers?page=1`)
+          .reply(200, [{ login: 'jseinfeld', id: 1 }, { login: 'gcostanza', id: 2 }]);
+
+        const wrapper = shallow(<App />);
+        wrapper.instance().searchForUser(goodUsername);
+      });
+
+      it('should set userFound state to true', () => {
+        // need setTimeout to process next tick
+        setTimeout(() => {
+          expect(wrapper.state('userFound')).toEqual(true);
+        }, 0);
+      });
+
+      it('should set followers state to response value', () => {
+        // need setTimeout to process next tick
+        setTimeout(() => {
+          expect(wrapper.state('followers')).toHaveLength(2);
+        }, 0);
+      });
+    });
   });
 });
