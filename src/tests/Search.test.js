fb0789536ba943e8002904b8ce546e19cb9060a0
Adam Loftin <loftin.adam@gmail.com>
2017-10-05 13:08:16 -0500

    Test for Search render

--- src/tests/Search.test.js
+++ src/tests/Search.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { shallow, find, to, have, length } from 'enzyme';
 import Search from '../components/Search';
+import SearchInput from '../components/SearchInput';
+import Button from '../components/Button';
 
 describe('Search', () => {
+  it('should render SearchInput and Button', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper.containsAllMatchingElements([<SearchInput />, <Button />])).toBe(true);
+  });
+
   it('should have an empty initial searchText state', () => {
     const wrapper = shallow(<Search />);
     expect(wrapper.state().searchText).toEqual('');
