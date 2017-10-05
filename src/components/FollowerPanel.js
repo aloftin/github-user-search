d760f250713e580e3ddc6ba4959cc16ef06f1c2a
Adam Loftin <loftin.adam@gmail.com>
2017-10-05 17:45:25 -0500

    Reworked the look of the FollowerPanels

--- src/components/FollowerPanel.js
+++ src/components/FollowerPanel.js
@@ -3,16 +3,14 @@ import Avatar from './Avatar';
 import styled from 'styled-components';
 
 const Panel = styled.div`
-  height: 40px;
-  min-width: 15rem;
+  display: inline-block;
+  width: 104px;
   background-color: #ffffff;
+  color: #082746;
   border-radius: 5px;
   box-shadow: 0px 5px 15px -3px rgba(8, 39, 70, 0.14);
-  margin: 0px auto 5px auto;
-  padding: 5px;
-  text-align: left;
-  display: flex;
-  align-items: center;
+  margin: 3px;
+  padding: 7px 5px 5px 5px;
   transform: perspective(1px) translateZ(0);
   transition-duration: 0.3s;
   transition-property: transform;
@@ -23,19 +21,24 @@ const Panel = styled.div`
   :focus,
   :active {
     transform: scale(1.1);
+    text-decoration: none;
   }
 
-  span {
-    margin-left: 5px;
+  .username {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 `;
 
-const FollowerPanel = ({ username, avatarUrl }) => {
+const FollowerPanel = ({ avatarUrl, username, profileUrl }) => {
   return (
-    <Panel>
-      <Avatar url={avatarUrl} />
-      <span>{username}</span>
-    </Panel>
+    <a href={profileUrl} title={username}>
+      <Panel>
+        <Avatar url={avatarUrl} />
+        <div className="username">{username}</div>
+      </Panel>
+    </a>
   );
 };
 
--- src/components/Followers.js
+++ src/components/Followers.js
@@ -4,12 +4,27 @@ import FollowerPanel from './FollowerPanel';
 class Followers extends React.Component {
   renderFollowerPanel(key) {
     const follower = this.props.followers[key];
-
-    return <FollowerPanel key={key} username={follower.login} avatarUrl={follower.avatar_url} />;
+    return (
+      <FollowerPanel
+        key={key}
+        avatarUrl={follower.avatar_url}
+        username={follower.login}
+        profileUrl={follower.html_url}
+      />
+    );
   }
 
   render() {
-    return <div>{Object.keys(this.props.followers).map(key => this.renderFollowerPanel(key))}</div>;
+    if (this.props.followers.length !== 0) {
+      return (
+        <div>
+          <div>Followers:</div>
+          <div>{Object.keys(this.props.followers).map(key => this.renderFollowerPanel(key))}</div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
   }
 }
 
